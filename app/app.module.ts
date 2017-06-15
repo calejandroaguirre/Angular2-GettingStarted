78d735de86d34acadf77c225df8eee7a6e2b83a6
CarlosAguirre <calejandroaguirre@outlook.com>
2017-06-14 19:16:53 -0600

    -Nested component -Added filter pipe -Added input and output for nested component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }  from './app.component';
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from './app.component';
 import { ProductListComponent } from "./products/product-list.component";
+import { ProductFilterPipe } from "./products/product-filter-pipe";
+import { StarComponent } from "./shared/star.component";
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ 
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
     AppComponent,
-    ProductListComponent
-     ],
-  bootstrap: [ AppComponent ]
+    ProductListComponent,
+    ProductFilterPipe,
+    StarComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
